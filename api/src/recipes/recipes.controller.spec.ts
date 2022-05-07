838074acb10bc84425990aba548189ae1283179b
George Gebbett <george.gebbett1@gmail.com>
2022-05-07 16:03:41 +0100

    making a start on some tests for the recipe controller

--- api/src/recipes/recipes.controller.spec.ts
+++ api/src/recipes/recipes.controller.spec.ts
@@ -1,7 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecipesController } from './recipes.controller';
 import { RecipesService } from './recipes.service';
-import { RecipesModule } from './recipes.module';
+
+const recipeModel = {
+  findOne: jest.fn().mockImplementation((id: string) =>
+    Promise.resolve({
+      _id: id,
+    }),
+  ),
+};
 
 describe('RecipesController', () => {
   let recipesController: RecipesController;
@@ -10,14 +17,26 @@ describe('RecipesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RecipesController],
-      providers: [RecipesService, RecipesModule],
+      providers: [{ provide: RecipesService, useValue: recipeModel }],
+      imports: [],
     }).compile();
 
     recipesController = module.get<RecipesController>(RecipesController);
-    // recipesService = module.get<RecipesService>(RecipesService);
+    recipesService = module.get<RecipesService>(RecipesService);
   });
 
   it('should be defined', () => {
     expect(recipesController).toBeDefined();
+  });
+
+  describe('getById', () => {
+    it('should return a single recipe', () => {
+      expect(recipesController.findOne('1')).resolves.toEqual({
+        _id: '1',
+      });
+      expect(recipesController.findOne('2')).resolves.toEqual({
+        _id: '2',
+      });
+    });
   });
 });
