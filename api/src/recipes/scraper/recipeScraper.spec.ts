52e392f13d4939b65d9ceb8f0587cf2c4e5b6e93
George Gebbett <george.gebbett1@gmail.com>
2022-05-13 12:19:50 +0100

    Add first scraper tests with 64% coverage

---
+++ api/src/recipes/scraper/recipeScraper.spec.ts
@@ -0,0 +1,160 @@
+import { RecipeScraper } from './recipeScraper';
+import { Test, TestingModule } from '@nestjs/testing';
+import { JSDOM } from 'jsdom';
+import { Recipe } from '../schemas/recipe.schema';
+
+describe('RecipeScraper', () => {
+  let scraper: RecipeScraper;
+
+  const belliniUrl = 'https://www.bbcgoodfood.com/recipes/bellini';
+
+  const belliniNodeList = () => {
+    const domString =
+      '<script type="application/ld+json">{"@context":"https://schema.org","@id":"https://www.bbcgoodfood.com/recipes/bellini#Recipe","@type":"Recipe","description":"A classy cocktail served in an elegant flute - this simple combination of peach purée and Prosecco makes a great start to any celebration","image":{"@type":"ImageObject","height":400,"url":"https://images.immediate.co.uk/production/volatile/sites/30/2020/08/bellini-b049342.jpg","width":440},"mainEntityOfPage":{"@type":"WebPage","@id":"https://www.bbcgoodfood.com/recipes/bellini"},"name":"Bellini","url":"https://www.bbcgoodfood.com/recipes/bellini","author":{"@type":"Person","name":"Good Food team"},"dateModified":"2020-08-08T02:26:26+00:00","datePublished":"2013-11-18T16:37:44+00:00","headline":"Bellini","keywords":"Christmas, Christmas morning, cocktails canapes, Good Food, Party, sparkling cocktail","publisher":{"@type":"Organization","name":"BBC Good Food","url":"https://www.bbcgoodfood.com","logo":{"@type":"ImageObject","url":"https://images.immediate.co.uk/production/volatile/sites/30/2019/07/GoodFood-dark-516d417.png","width":221,"height":58}},"nutrition":{"@type":"NutritionInformation","calories":"143 calories","carbohydrateContent":"18 grams carbohydrates","sugarContent":"18 grams sugar","fiberContent":"0.7 grams fiber","proteinContent":"0.7 grams protein"},"prepTime":"PT5M","recipeCategory":"Cocktails","recipeIngredient":["500ml peach purée or peach nectar","1 bottle prosecco"],"recipeInstructions":[{"@type":"HowToStep","text":"Put the peach puree in a Champagne flute up to about 1/3 full and slowly top up with Prosecco."}],"recipeYield":6,"totalTime":"PT5M"}</script><script type="application/ld+json">{"@context":"https://schema.org/","@type":"BreadcrumbList","itemListElement":[{"@type":"ListItem","position":1,"name":"Home","item":"https://www.bbcgoodfood.com/"},{"@type":"ListItem","position":2,"name":"Recipes","item":"https://www.bbcgoodfood.com/recipes"},{"@type":"ListItem","position":3,"name":"Bellini"}]}</script>{"@context":"https://schema.org/","@id":"https://www.bbcgoodfood.com/recipes/bellini#Recipe","aggregateRating":{"@type":"AggregateRating","ratingValue":4,"reviewCount":2,"bestRating":5,"worstRating":1}}';
+    return JSDOM.fragment(domString).querySelectorAll(
+      "script[type='application/ld+json']",
+    );
+  };
+
+  const belliniMetadataObject = {
+    '@context': 'https://schema.org',
+    '@id': 'https://www.bbcgoodfood.com/recipes/bellini#Recipe',
+    '@type': 'Recipe',
+    description:
+      'A classy cocktail served in an elegant flute - this simple combination of peach purée and Prosecco makes a great start to any celebration',
+    image: {
+      '@type': 'ImageObject',
+      height: 400,
+      url: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/bellini-b049342.jpg',
+      width: 440,
+    },
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': 'https://www.bbcgoodfood.com/recipes/bellini',
+    },
+    name: 'Bellini',
+    url: 'https://www.bbcgoodfood.com/recipes/bellini',
+    author: { '@type': 'Person', name: 'Good Food team' },
+    dateModified: '2020-08-08T02:26:26+00:00',
+    datePublished: '2013-11-18T16:37:44+00:00',
+    headline: 'Bellini',
+    keywords:
+      'Christmas, Christmas morning, cocktails canapes, Good Food, Party, sparkling cocktail',
+    publisher: {
+      '@type': 'Organization',
+      name: 'BBC Good Food',
+      url: 'https://www.bbcgoodfood.com',
+      logo: {
+        '@type': 'ImageObject',
+        url: 'https://images.immediate.co.uk/production/volatile/sites/30/2019/07/GoodFood-dark-516d417.png',
+        width: 221,
+        height: 58,
+      },
+    },
+    nutrition: {
+      '@type': 'NutritionInformation',
+      calories: '143 calories',
+      carbohydrateContent: '18 grams carbohydrates',
+      sugarContent: '18 grams sugar',
+      fiberContent: '0.7 grams fiber',
+      proteinContent: '0.7 grams protein',
+    },
+    prepTime: 'PT5M',
+    recipeCategory: 'Cocktails',
+    recipeIngredient: [
+      '500ml peach purée or peach nectar',
+      '1 bottle prosecco',
+    ],
+    recipeInstructions: [
+      {
+        '@type': 'HowToStep',
+        text: 'Put the peach puree in a Champagne flute up to about 1/3 full and slowly top up with Prosecco.',
+      },
+    ],
+    recipeYield: 6,
+    totalTime: 'PT5M',
+  };
+
+  const belliniRecipe = new Recipe();
+  belliniRecipe.name = 'Bellini';
+  belliniRecipe.url = 'https://www.bbcgoodfood.com/recipes/bellini';
+  belliniRecipe.imageUrl =
+    'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/bellini-b049342.jpg';
+  belliniRecipe.ingredients = [
+    '500ml peach purée or peach nectar',
+    '1 bottle prosecco',
+  ];
+  belliniRecipe.steps = [
+    'Put the peach puree in a Champagne flute up to about 1/3 full and slowly top up with Prosecco.',
+  ];
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [RecipeScraper],
+    }).compile();
+    scraper = module.get<RecipeScraper>(RecipeScraper);
+  });
+
+  it('should be defined', function () {
+    expect(scraper).toBeDefined();
+  });
+
+  it('should get a NodeList from a given URL', async function () {
+    jest
+      .spyOn(scraper, 'getNodeListOfMetadataNodesFromUrl')
+      .mockResolvedValueOnce(belliniNodeList());
+    const foundNodeList = await scraper.getNodeListOfMetadataNodesFromUrl(
+      belliniUrl,
+    );
+    expect(foundNodeList).toEqual(belliniNodeList());
+  });
+
+  it('should extract the Recipe object from a NodeList', function () {
+    expect(scraper.getSchemaRecipeFromNodeList(belliniNodeList())).toEqual(
+      belliniMetadataObject,
+    );
+  });
+
+  it('should correctly parse a recipe name', function () {
+    expect(scraper.parseRecipeName(belliniMetadataObject.name)).toEqual(
+      'Bellini',
+    );
+  });
+
+  it('should correctly parse recipe steps', function () {
+    expect(
+      scraper.parseRecipeSteps(belliniMetadataObject.recipeInstructions),
+    ).toEqual([
+      'Put the peach puree in a Champagne flute up to about 1/3 full and slowly top up with Prosecco.',
+    ]);
+  });
+
+  it('should correctly extract the recipe image url', function () {
+    expect(scraper.parseImageUrl(belliniMetadataObject.image)).toEqual(
+      belliniMetadataObject.image.url,
+    );
+  });
+
+  it('should correctly parse recipe ingredients', function () {
+    const parsedIngredients = scraper.parseRecipeIngredients(
+      belliniMetadataObject.recipeIngredient,
+    );
+
+    expect(parsedIngredients).toEqual([
+      '500ml peach purée or peach nectar',
+      '1 bottle prosecco',
+    ]);
+  });
+
+  it('should be able to return a recipe object from a url', async function () {
+    jest
+      .spyOn(scraper, 'getNodeListOfMetadataNodesFromUrl')
+      .mockResolvedValueOnce(belliniNodeList());
+
+    expect(await scraper.hydrateRecipe(belliniUrl)).toEqual(belliniRecipe);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});
