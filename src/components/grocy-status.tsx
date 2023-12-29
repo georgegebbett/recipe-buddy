2b3e09be48c1010442975ecd58f66601bdee881f
George Gebbett <george.gebbett1@gmail.com>
2023-12-29 21:03:41 +0000

    Add loading skeleton to GrocyStatus component
    -added loading skeleton to the GrocyStatus component to enhance user experience during data fetching. Loading skeleton animations will show during the data fetching process before the final data is received and rendered, improving the perceived performance. This update also includes minor code formatting adjustments for better readability.

--- src/components/grocy-status.tsx
+++ src/components/grocy-status.tsx
@@ -1,32 +1,42 @@
 "use client"
 
-import { api } from "~/trpc/react"
-import { CheckCircle } from "lucide-react"
+import {api} from "~/trpc/react"
+import {CheckCircle} from "lucide-react"
 
-import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert"
+import {Alert, AlertDescription, AlertTitle} from "~/components/ui/alert"
+import {Skeleton} from "~/components/ui/skeleton";
 
 export const GrocyStatus = () => {
-  const { data } = api.grocy.checkConnection.useQuery()
+  const {data, isLoading} = api.grocy.checkConnection.useQuery()
 
   return (
-    <>
-      {data && data.success ? (
-        <Alert>
-          <CheckCircle className="h-4 w-4" />
-          <AlertTitle>Connected to Grocy</AlertTitle>
-          <AlertDescription>
-            Connected to Grocy v{data.data.grocy_version.Version}
-          </AlertDescription>
-        </Alert>
-      ) : (
-        <Alert>
-          <CheckCircle className="h-4 w-4" />
-          <AlertTitle>Not connected to Grocy</AlertTitle>
-          <AlertDescription>
-            Unable to connect to Grocy - check your URL and API key
-          </AlertDescription>
-        </Alert>
-      )}
-    </>
+      <>
+        {data && data.success ? (
+            <Alert>
+              <CheckCircle className="h-4 w-4"/>
+              <AlertTitle>Connected to Grocy</AlertTitle>
+              <AlertDescription>
+                Connected to Grocy v{data.data.grocy_version.Version}
+              </AlertDescription>
+            </Alert>
+        ) : isLoading ? (
+            <Alert className="pl-10">
+              <AlertTitle>
+                <Skeleton className="w-40 h-5"/>
+              </AlertTitle>
+              <AlertDescription>
+                <Skeleton className="w-80 h-4"/>
+              </AlertDescription>
+            </Alert>
+        ) : (
+            <Alert>
+              <CheckCircle className="h-4 w-4"/>
+              <AlertTitle>Not connected to Grocy</AlertTitle>
+              <AlertDescription>
+                Unable to connect to Grocy - check your URL and API key
+              </AlertDescription>
+            </Alert>
+        )}
+      </>
   )
 }
