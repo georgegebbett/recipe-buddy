63944bcaf014134427178b6ceb5c7ffed08c1a6c
George Gebbett <george.gebbett1@gmail.com>
2025-03-17 12:27:03 +0700

    refactor: fix typecheck

--- src/components/empty-placeholder.tsx
+++ src/components/empty-placeholder.tsx
@@ -43,7 +43,6 @@ EmptyPlaceholder.Icon = function EmptyPlaceHolderIcon({
 
   return (
     <div className="flex size-20 items-center justify-center rounded-full bg-muted">
-      {/*@ts-expect-error Icons are problematic*/}
       <Icon className={cn("size-10", className)} {...props} />
     </div>
   )
